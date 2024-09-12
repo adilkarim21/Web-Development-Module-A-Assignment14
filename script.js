2e60058b90b88b0d313ec48a82e4c75932d549e7
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-09-12 15:09:48 +0500

    Only JS

---
+++ script.js
@@ -0,0 +1,246 @@
+// Home Assignment:
+
+
+// Chapter 6 (Math Expression II)
+
+// Q1:
+// var x = "1";
+// x = ++x
+// document.write(x)
+
+// Q2:
+// var x = "100";
+// x = --x
+// document.write(x)
+
+// Q3:
+// var x = "50";
+// var y = x++
+// document.write("the value of y is " + y)
+
+// Q4:
+// var y = "50";
+// var z = --y
+// document.write("the value of z is " + z)
+
+// Q5:
+// var num = "5";
+// var newNum = --num
+// document.write("newNum = " + newNum)
+
+// Q6:
+// var x = "10";
+// var z = ++x
+// document.write("z = " + z)
+
+// Q7:  
+// var z = "50";
+// z = ++z
+// alert(z)
+
+
+// Chapter 7 (Math Expression III)
+
+// Q1:
+// var calculatedNum = 2 + (2 * 6);
+// document.write("the value of calculatedNum is " + calculatedNum)
+
+// Q2:
+// var calculatedNum = (2 + 2) * 6;
+// document.write("the value of calculatedNum is " + calculatedNum)
+
+// Q3:
+// var calculatedNum = (2 + 2) * (4 + 2);
+// document.write("the value of calculatedNum is " + calculatedNum)
+
+// Q4:
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// document.write("the value of calculatedNum is " + calculatedNum)
+
+// Q5:
+
+// Q6:
+
+// Q7:
+
+
+
+// Chapter 8 (Concatenating Text Strings)
+
+// Q1:
+// var num = "2" + "2";
+// document.write("the value of num " + num)
+
+// Q2:
+// var message = ("Hello," + "Dolly");
+// alert("the value of message is " + message)
+
+// Q3:
+// alert("33" + 3);
+
+// Q4:
+
+// Q5:
+
+// Q6:
+
+
+
+// Chapter 9 (Prompts)
+
+// Q1:
+// var firstName = prompt("Enter first name")
+
+// Q2:
+
+// Q3:
+// var yourName = prompt("Enter Your Name");
+
+// Q4:
+
+// Q5:
+
+// Q6:
+
+
+
+// Class Assignment:
+
+
+// Chap 1 (MATH EXPRESSIONS)
+
+// Q1:
+// var num1 = prompt("Enter 1st number");
+// var num2 = prompt("Enter 2nd number");
+// var sum = num1 + num2;
+// document.write("Sum of " , num1 , " & " , num2 , " is " , sum);
+
+// Q2:
+// var sub = num1 - num2;
+// var multi = num1 * num2;
+// var div = num1 / num2;
+// var mod = num1 % num2;
+// document.write("Subtraction of " , num1 , " & " , num2 , " is " , sub);
+// document.write("Multiplication of " , num1 , " & " , num2 , " is " , multi);
+// document.write("Division of " , num1 , " & " , num2 , " is " , div);
+// document.write("Modulus of " , num1 , " & " , num2 , " is " , mod);
+
+// Q3:
+// var value 
+// document.write("Value after variable declaration is " , value);
+// value = 5;
+// document.write("Initialization value: " , value);
+// value = ++value;
+// document.write("Value after increment is: " , value);
+// value = 7 + value;
+// document.write("Value after addition is: " , value);
+// value = --value;
+// document.write("Value after decrement is: " , value);
+// value = value % 3;
+// document.write("The remainder is: " , value);
+
+// Q4:
+// var cost = 600;
+// var numberOfTicket = prompt("Enter number of tickets you want:");
+// var totalCost = cost * numberOfTicket;
+// document.write("Total cost to buy " , numberOfTicket , " tickets to a movie is " , totalCost , "PKR");
+
+// Q5:
+// var numberOfTable = prompt("Enter number of table you want:")
+// document.write("Table of " , numberOfTable)
+
+// Q6:
+// var celsius = prompt("Enter celsius value: ");
+// var fahrenheit = prompt("Enter fahrenheit value: ");
+// var newCelsius , newFahrenheit;
+// newCelsius = ( fahrenheit - 32 ) * 5 / 9;
+// newFahrenheit = ( celsius * 9 / 5 ) + 32;
+// document.write(celsius , "C is " , newFahrenheit , "F");
+// document.write(fahrenheit , "F is " , newCelsius , "C");
+
+// Q7:
+// var item1 = 650;
+// var item2 = 100;
+// var shippingCharges = 100;
+// var quantityOfItem1 = prompt("Enter quantity of item 1: ");
+// var quantityOfItem2 = prompt("Enter quantity of item 2: ");
+// document.write("Price of item 1 is " , item1);
+// document.write("Quantity of item 1 is " , quantityOfItem1);
+// document.write("Price of item 2 is " , item2);
+// document.write("Quantity of item 2 is " , quantityOfItem2);
+// document.write("Shipping Charges " , shippingCharges);
+// document.write("Total cost of your order is " , item1 * quantityOfItem1 + item2 * quantityOfItem2 + shippingCharges);
+
+// Q8:
+// var totalMarks = 980;
+// var marksObtained = prompt("Enter obtained marks: ");
+// var percentage = ( marksObtained * 100 ) / totalMarks;
+// document.write("Total Marks: " , totalMarks);
+// document.write("Marks Obtained: " , marksObtained);
+// document.write("Percentage: " , percentage);
+
+// Q9:
+// var usDollars = 10;
+// var saudiRiyals = 25;
+// var amount1 = 104.80 * usDollars;
+// var amount2 = 28 * saudiRiyals;
+// var total = amount1 + amount2;
+// document.write("Total Currency in PKR: " , total);
+
+// Q10:
+// var value = 3;
+// value = ( ( value + 5 ) * 10 ) / 2;
+
+// Q11:
+// var currentYear = prompt("Enter current year: ");
+// var birthYear =prompt("Enter birth year: ");
+// var age = currentYear - birthYear;
+// document.write("Current Year: " , currentYear);
+// document.write("Birth Year :" , birthYear);
+// document.write("Your Age is: " , age);
+
+// Q12:
+
+// Q13:
+
+
+// Chapter 6-9 (MATH EXPRESSIONS)
+
+// Q1:
+// var a = prompt("Enter a number: ");
+// document.write("Result:");
+// document.write("The value of a is: " , a);
+// document.write("..............................")
+// a = ++a;
+// document.write("The value of ++a is: " , a);
+// document.write("Now the value of a is: " , a);
+// a = a++;
+// document.write("The value of a++ is: " , a);
+// document.write("Now the value of a is: " , a);
+// a = --a;
+// document.write("The value of --a is: " , a);
+// document.write("Now the value of a is: " , a);
+// a = a--;
+// document.write("The value of a-- is: " , a);
+// document.write("Now the value of a is: " , a);
+
+// Q2:
+// var a = 2 , b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(
+//     1;
+//     1 - 0;
+//     1 - 0 + 1;
+//     1 - 0 + 1 + 1;
+//     "a is " , 1
+//     "b is " , 1
+//     "result is " , result
+//     )
+
+// Q3:
+// var userName = prompt("Enter your name: ");
+// alert("Welcome To Our WebPage :) ");
+
+// Q5:
+
+// Q6:
